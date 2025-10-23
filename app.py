9825c1a212b829dead3b506c45f147eeadff6457
yencan65 <e186541@gmail.com>
2025-10-23 11:52:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -133,3 +133,18 @@ def recommendations():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "5000"))
     app.run(host="0.0.0.0", port=port)
+    from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "https://findalleasy.com",
+    "https://www.findalleasy.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
