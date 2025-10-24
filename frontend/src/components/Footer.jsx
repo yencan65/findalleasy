ffd544aa7d3200e7f69fef887df670c2c098e3f4
yencan65 <e186541@gmail.com>
2025-10-24 05:50:41 +0300

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-  return (
-    <footer className="footer">
-      <p>©2025 FindAllEasy — Yapay Zeka Destekli Global Alışveriş Asistanın.</p>
-      <p>Onun uygun fiyatı bulması, parmak şıklatman kadar kolay. ✨</p>
-    </footer>
-  )
-}
