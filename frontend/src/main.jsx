f3caba2e3bc91bf66e1a2b3b7afe90c7505c583b
yencan65 <e186541@gmail.com>
2025-10-25 10:51:21 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home.jsx'
+import "./lang/i18n";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
