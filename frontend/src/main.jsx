9c06bddf7c2a34fd19df1b4261cc71f40e694b69
yencan65 <e186541@gmail.com>
2025-10-25 10:54:16 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home.jsx'
+
 import "./lang/i18n";
+
+import AIAsistan from "./components/AIAsistan.jsx";
+import { useTranslation } from "react-i18next";
+const { t } = useTranslation();
+
+// JSX içinde:
+<AIAsistan t={t} />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
